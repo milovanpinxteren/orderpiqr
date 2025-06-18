df87c46f16f1b6c26ae25c61a03f828f0071f500
Milo <milo.van.pinxteren@gmail.com>
2025-06-18 12:09:24 +0200

    feat: improve styling

--- orderpiqrApp/static/orderpiqrApp/js/qrScanner.js
+++ orderpiqrApp/static/orderpiqrApp/js/qrScanner.js
@@ -3,7 +3,7 @@
 export function initializeScanner(onScanCallback) {
     const qrCodeScanner = new Html5QrcodeScanner("reader", {
         fps: 10,  // Frames per second
-        qrbox: 250,  // Size of the scanning area
+        qrbox: 200,  // Size of the scanning area
         readers: ["qr_code_reader", "ean_reader", "upc_reader", "code_128_reader"]
     });
 
