9debb0c2c47be2c8c948014d14805309ea7f49ea
Milo <milo.van.pinxteren@gmail.com>
2025-06-25 14:38:25 +0200

    feat: improve styling

--- orderpiqrApp/static/orderpiqrApp/js/qrScanner.js
+++ orderpiqrApp/static/orderpiqrApp/js/qrScanner.js
@@ -1,9 +1,11 @@
 // qrScanner.js
 
 export function initializeScanner(onScanCallback) {
+    const width = window.innerWidth;
+    const qrBoxSize = Math.min(width * 0.65, 350); // max 300px, but responsive
     const qrCodeScanner = new Html5QrcodeScanner("reader", {
         fps: 5,  // Frames per second
-        qrbox: 250,  // Size of the scanning area
+        qrbox: { width: qrBoxSize, height: qrBoxSize },
         aspectRatio: 1.0,
         readers: ["qr_code_reader", "ean_reader", "upc_reader", "code_128_reader"]
     });
