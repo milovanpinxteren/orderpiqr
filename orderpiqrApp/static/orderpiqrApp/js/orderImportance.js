546bc8df334172df435b84ea69ba86aee54ddd71
Milo <milo.van.pinxteren@gmail.com>
2025-08-08 15:12:47 +0200

    feat: change sorting on button press

--- orderpiqrApp/static/orderpiqrApp/js/orderImportance.js
+++ orderpiqrApp/static/orderpiqrApp/js/orderImportance.js
@@ -3,7 +3,6 @@ const gettext = window.gettext;  // Pull it from the global scope
 // let isOrderImportant = true;  // Flag to track if order matters
 
 export let isOrderImportant = window.SETTINGS?.order_importance ?? true;
-console.log('isorderimportant', isOrderImportant)
 
 export function toggleOrderImportance() {
     isOrderImportant = !isOrderImportant;
@@ -12,13 +11,10 @@ export function toggleOrderImportance() {
 
 export function updateOrderImportanceButton(button) {
     if (isOrderImportant) {
-        console.log('order is important')
         button.textContent = gettext('Order Importance: Enabled');
         button.style.backgroundColor = "#28a745";  // Green for enabled
         isOrderImportant = true
     } else {
-        console.log('order is not important')
-
         button.textContent = gettext('Order Importance: Disabled');
         button.style.backgroundColor = "#dc3545";  // Red for disabled
         isOrderImportant = false
